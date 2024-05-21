e66a233972732fb5b3baf792421ca089875cecfc
Anastasiya Elkafrawi <atverdova78@gmail.com>
2024-05-21 12:00:48 +0400

    Lesson_7 tests on junit5 completed

--- homework/src/test/java/Tests.java
+++ homework/src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import school.lessons.Lesson_7_junit_5;
 
@@ -6,17 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Tests {
 
     @Test
+    @DisplayName("Прямой позитивный кейс, вычислить факториал")
     public void testFactorialForPositiveNumber(){
         int result = Lesson_7_junit_5.getFactorial(5);
         assertEquals(120, result);
     }
     @Test
+    @DisplayName("Факториал 0 = 1")
     public void testFactorialForZero() {
         int result = Lesson_7_junit_5.getFactorial(0);
         assertEquals(1, result);
     }
 
     @Test
+    @DisplayName("Нельзя посчитать факториал для отрицательного числа")
     void exceptionTesting() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Lesson_7_junit_5.getFactorial(-2),
                 "Expected getFactorial() to throw, but it didn't");
