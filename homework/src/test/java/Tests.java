3054f9fafbb2c05a28bb12700a4a46e21645b19c
Anastasiya Elkafrawi <atverdova78@gmail.com>
2024-05-23 00:23:18 +0400

    Lesson_7_testng completed

--- homework/src/test/java/Tests.java
+++ homework/src/test/java/Tests.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.testng.annotations.Test;
 import school.lessons.Lesson_7_testng;
 
@@ -6,17 +7,20 @@ import static org.testng.Assert.assertEquals;
 public class Tests {
 
     @Test
+    @DisplayName("Прямой позитивный кейс, вычислить факториал")
     public void testFactorialForPositiveNumber(){
         int result = Lesson_7_testng.getFactorial(5);
         assertEquals(120, result);
     }
     @Test
+    @DisplayName("Факториал 0 = 1")
     public void testFactorialForZero() {
         int result = Lesson_7_testng.getFactorial(0);
         assertEquals(1, result);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
+    @DisplayName("Нельзя посчитать факториал для отрицательного числа")
     public void testFactorialForNegativeNumber() {
         Lesson_7_testng.getFactorial(-2);}
 }
