e2c734a46cfc28be64c7cb6956e5725d1bfa3f05
Anastasiya Elkafrawi <atverdova78@gmail.com>
2024-05-27 09:53:29 +0400

    Lesson_5 junit 5

--- homework/src/main/java/school/lessons/lesson_4/exercise_2/Shape.java
+++
@@ -1,7 +0,0 @@
-package school.lessons.lesson_4.exercise_2;
-
-public interface Shape {
-    double calculatePerimeter();
-    double calculateArea();
-    void setColor(String fillColor, String borderColor);
-}
