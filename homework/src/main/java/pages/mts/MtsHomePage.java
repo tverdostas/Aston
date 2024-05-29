8dba1c50586088c4368becf9d137315a40110447
Anastasiya Elkafrawi <atverdova78@gmail.com>
2024-05-30 00:44:49 +0400

    Lesson_11 page object refactoring in progress

--- homework/src/main/java/pages/mts/MtsHomePage.java
+++ homework/src/main/java/pages/mts/MtsHomePage.java
@@ -1,5 +1,6 @@
 package pages.mts;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.base.BasePage;
@@ -9,64 +10,148 @@ public class MtsHomePage extends BasePage {
     public MtsHomePage(WebDriver driver) {
         super(driver);
     }
-    private final By cookieAgree  = By.id("cookie-agree");
+
+    private final By cookieAgree = By.id("cookie-agree");
     private final By phoneNumberField = By.cssSelector("#connection-phone");
     private final By homeInternetField = By.xpath("//li/p[contains(text(), 'Домашний интернет')]");
     private final By openArrow = By.cssSelector(".select__arrow");
-    private final By installmentPlanButton = By.cssSelector("#score-instalment");
-    private final By debtButton = By.xpath("//li/p[contains(text(), 'Задолженность')]");
+    // private final By installmentPlanButtonField = By.cssSelector("#score-instalment");
+    private final By debtChoice = By.xpath("//li/p[contains(text(), 'Задолженность')]");
 
+    private final By installmentPlanChoice = By.xpath("//li/p[contains(text(), 'Рассрочка')]");
+
+    private final By sumField = By.cssSelector("#connection-sum");
+
+    private final By emailField = By.cssSelector("#connection-email");
+    private final String placeholderPhoneNumberField = "Номер телефона";
+    private final String placeholderEmailField = "E-mail для отправки чека";
+    private final String placeholderSumField = "Сумма";
+    private final String placeholderScoreInstalmentSumField = "Номер счета на 44";
+    private final String placeholderDebtSumField = "Номер счета на 2073";
+    private final String placeholderAbonentNumber = "Номер абонента";
+    private final By abonentNumber = By.cssSelector("#internet-phone");
+
+    private final By accountNumberField = By.cssSelector("#score-instalment");
+
+    private final By placeholderAccountNumber = By.cssSelector("#score-instalment");
+
+    public String getPlaceholderDebtSumField() {
+        return placeholderDebtSumField;
+    }
+    public String getPlaceholderAbonentNumber() {
+        return placeholderAbonentNumber;
+    }
+
+    public String getPlaceholderEmailField() {
+        return placeholderEmailField;
+    }
+
+    public String getPlaceholderSumField() {
+        return placeholderSumField;
+    }
+
+
+    public By getPlaceholderAccountNumber() {
+        return placeholderAccountNumber;
+    }
+
+    @Step("Нажатие на Принять в окне Обработка файлов cookie")
+    // думаю, окно можно вынести отдельным блоком и расписать кнопки и степы для него
     public MtsHomePage clickCookieAgree() {
         driver.findElement(cookieAgree).click();
         return this;
     }
-    public By getPhoneNumberField() {
+
+/*    public By getPhoneNumberField() {
         return phoneNumberField;
-    }
+    }*/
 
     public String getPlaceholderPhoneNumberField() {
         return placeholderPhoneNumberField;
     }
 
-    private final String placeholderPhoneNumberField = "Номер телефона";
-
-    public MtsHomePage findPhoneNumberField() {
+/*    public MtsHomePage findPhoneNumberField() {
         driver.findElement(phoneNumberField).click();
         return this;
-    }
+    }*/
 
+    @Step("Получение плейсхолдера поля Номер телефона")
     public String getPlaceholderPhoneNumberFieldFromPage() {
         String value = driver.findElement(phoneNumberField).getAttribute("placeholder");
         return value;
     }
 
+    @Step("Получение плейсхолдера поля E-mail")
+    public String readPlaceholderEmail() {
+        String value = driver.findElement(emailField).getAttribute("placeholder");
+        return value;
+    }
+
+    @Step("Получение плейсхолдера поля Сумма")
+    public String readPlaceholderSumField() {
+        String value = driver.findElement(sumField).getAttribute("placeholder");
+        return value;
+    }
+
+    @Step("Получение плейсхолдера поля Номер абонента")
+    public String readPlaceholderAbonentNumber() {
+        String value = driver.findElement(abonentNumber).getAttribute("placeholder");
+        return value;
+    }
+
+    @Step("Получение плейсхолдера поля Номер счёта")
+    public String readPlaceholderAccountNumber() {
+        String value = driver.findElement(accountNumberField).getAttribute("placeholder");
+        return value;
+    }
+
+    @Step("Получение плейсхолдера поля Номер счета на 2073")
+    public String readPlaceholderAccountNumberDebt() {
+        String value = driver.findElement(accountNumberField).getAttribute("placeholder");
+        return value;
+    }
 
 /*    public MtsHomePage fillPhoneNumber(String phoneNumber){
         driver.findElement(phoneNumberField).click();
         return this;
     }*/
 
-    public MtsHomePage findHomeInternet() {
+    @Step("Выбрать из выпадающего списка Домашний интернет")
+    public void findHomeInternet() {
         driver.findElement(homeInternetField).click();
-        return this;
+    }
+    @Step("Выбрать из выпадающего списка Рассрочка")
+    public void chooseInstallmentPlan() {
+        driver.findElement(installmentPlanChoice).click();
+    }
+    @Step("Выбрать из выпадающего списка Задолженность")
+    public void chooseDebt() {
+        driver.findElement(debtChoice).click();
     }
 
-    public MtsHomePage openArrow() {
+    @Step("Нажать на стрелку выпадающего списка услуг")
+    public void openArrow() {
         driver.findElement(openArrow).click();
-        return this;
     }
 
-    public MtsHomePage findInstallmentPlan() {
-        driver.findElement(installmentPlanButton).click();
+
+/*    public MtsHomePage findInstallmentPlanField() {
+        driver.findElement(scoreInstalmentField).click();
         return this;
-    }
+    }*/
 
     public MtsHomePage findDebtButton() {
-        driver.findElement(debtButton).click();
+        driver.findElement(debtChoice).click();
         return this;
     }
 
 /*    public MtsHomePage clickToFind(){
         WebElement
     }*/
+
+    public MtsHomePage findSumField() {
+        driver.findElement(sumField).click();
+        return this;
+    }
+
 }
--- homework/src/test/java/school/lessons/tests/mts/HomePageTests.java
+++ homework/src/test/java/school/lessons/tests/mts/HomePageTests.java
@@ -1,16 +1,27 @@
 package school.lessons.tests.mts;
 
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.mts.MtsHomePage;
 import school.lessons.tests.base.BaseTest;
 
 import static constants.Constant.TimeoutVariable.Urls.MTS_HOME_PAGE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static school.lessons.common.CommonActions.closeDriver;
 
 public class HomePageTests extends BaseTest {
 
     @Test
+    @Epic("Сайт МТС")
+    @Feature("Главная страница")
+    @Story("Плейсхолдеры 'Услуги связи' верные")
+    @Description("Плейсхолдеры 'Услуги связи' верные")
+    @DisplayName("Плейсхолдеры 'Услуги связи' верные")
     public void checkPlaceHolderOfPhoneNumberFieldTest() {
         MtsHomePage mtsHomePage = new MtsHomePage(driver);
 
@@ -19,5 +30,107 @@ public class HomePageTests extends BaseTest {
         mtsHomePage.getPlaceholderPhoneNumberFieldFromPage();
 
         assertEquals(mtsHomePage.getPlaceholderPhoneNumberField(), mtsHomePage.getPlaceholderPhoneNumberFieldFromPage());
+
+        mtsHomePage.readPlaceholderSumField();
+
+        assertEquals(mtsHomePage.getPlaceholderSumField(), mtsHomePage.readPlaceholderSumField());
+
+        mtsHomePage.readPlaceholderEmail();
+
+        assertEquals(mtsHomePage.getPlaceholderEmailField(), mtsHomePage.readPlaceholderEmail());
+
+        closeDriver(driver);
+    }
+
+    @Test
+    @Epic("Сайт МТС")
+    @Feature("Главная страница")
+    @Story("Плейсхолдеры 'Домашний интернет' верные")
+    @Description("Плейсхолдеры 'Домашний интернет' верные")
+    @DisplayName("Плейсхолдеры 'Домашний интернет' верные")
+    public void checkPlaceHolderOfHomeInternetFieldTest() {
+        MtsHomePage mtsHomePage = new MtsHomePage(driver);
+
+        mtsHomePage.open(MTS_HOME_PAGE);
+        mtsHomePage.clickCookieAgree();
+
+        mtsHomePage.openArrow();
+        mtsHomePage.findHomeInternet();
+
+        mtsHomePage.readPlaceholderAbonentNumber();
+
+        assertEquals(mtsHomePage.getPlaceholderAbonentNumber(), mtsHomePage.readPlaceholderAbonentNumber());
+
+        mtsHomePage.readPlaceholderSumField();
+
+        assertEquals(mtsHomePage.getPlaceholderSumField(), mtsHomePage.readPlaceholderSumField());
+
+        mtsHomePage.readPlaceholderEmail();
+
+        assertEquals(mtsHomePage.getPlaceholderEmailField(), mtsHomePage.readPlaceholderEmail());
+
+        closeDriver(driver);
+    }
+
+    @Test
+    @Epic("Сайт МТС")
+    @Feature("Главная страница")
+    @Story("Плейсхолдеры 'Рассрочка' верные")
+    @Description("Плейсхолдеры 'Рассрочка' верные")
+    @DisplayName("Плейсхолдеры 'Рассрочка' верные")
+    public void checkPlaceHolderOfInstallmentPlanChoiceTest() {
+        MtsHomePage mtsHomePage = new MtsHomePage(driver);
+
+        mtsHomePage.open(MTS_HOME_PAGE);
+        mtsHomePage.clickCookieAgree();
+
+        mtsHomePage.openArrow();
+
+        mtsHomePage.chooseInstallmentPlan();
+
+        mtsHomePage.readPlaceholderAccountNumber();
+
+        assertEquals(mtsHomePage.getPlaceholderAccountNumber(), mtsHomePage.readPlaceholderAccountNumber());
+
+        mtsHomePage.readPlaceholderSumField();
+
+        assertEquals(mtsHomePage.getPlaceholderSumField(), mtsHomePage.readPlaceholderSumField());
+
+        mtsHomePage.readPlaceholderEmail();
+
+        assertEquals(mtsHomePage.getPlaceholderEmailField(), mtsHomePage.readPlaceholderEmail());
+
+        closeDriver(driver);
+    }
+
+    @Test
+    @Epic("Сайт МТС")
+    @Feature("Главная страница")
+    @Story("Плейсхолдеры 'Задолженность' верные")
+    @Description("Плейсхолдеры 'Задолженность' верные")
+    @DisplayName("Плейсхолдеры 'Задолженность' верные")
+    public void checkPlaceHolderOfDebtChoiceTest() {
+        MtsHomePage mtsHomePage = new MtsHomePage(driver);
+
+        mtsHomePage.open(MTS_HOME_PAGE);
+        mtsHomePage.clickCookieAgree();
+
+        mtsHomePage.openArrow();
+
+        mtsHomePage.chooseDebt();
+
+        mtsHomePage.readPlaceholderAccountNumberDebt();
+
+        assertEquals(mtsHomePage.getPlaceholderDebtSumField(), mtsHomePage.readPlaceholderAccountNumberDebt());
+
+        mtsHomePage.readPlaceholderSumField();
+
+        assertEquals(mtsHomePage.getPlaceholderSumField(), mtsHomePage.readPlaceholderSumField());
+
+        mtsHomePage.readPlaceholderEmail();
+
+        assertEquals(mtsHomePage.getPlaceholderEmailField(), mtsHomePage.readPlaceholderEmail());
+
+        closeDriver(driver);
     }
 }
